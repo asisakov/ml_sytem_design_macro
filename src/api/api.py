a5bcd2ddc3b9e4bd981a0b0de6060208690eb9ca
Пётр Щучкин <petr.schuchkin@rt.ru>
2023-11-30 18:07:07 +0300

    Добавил prefix=/api и обновил requirements

--- src/api/api.py
+++ src/api/api.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI
 import history
 import forecast
 
+PREFIX = '/api'
+
 logging.basicConfig(level='INFO',
                     format='%(asctime)s\t%(levelname)s\t%(message)s')
 
 app = FastAPI()
-app.include_router(history.router)
-app.include_router(forecast.router)
+app.include_router(history.router, prefix=PREFIX)
+app.include_router(forecast.router, prefix=PREFIX)
